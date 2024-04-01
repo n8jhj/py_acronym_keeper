28eaea4967bb25e5c4b3b7607f001f447d0dba9d
Nathaniel Jones <nathaniel_jones@selinc.com>
2024-04-01 16:21:37 -0700

    Wire up CLI

---
+++ py_acronym_keeper/cli/root.py
@@ -0,0 +1,15 @@
+"""Root of the CLI."""
+
+import click
+
+
+@click.group
+def root_group():
+    """\b
+     _  _ |
+    |_)(_||<      Py Acronym Keeper
+    |
+    """
+    # Thanks to Patrick Gillespie for the ASCII art:
+    # https://patorjk.com/software/taag/#p=display&f=Three%20Point&t=pak
+    ...
