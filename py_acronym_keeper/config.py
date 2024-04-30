7c88a4b8bbc60484af1633018dec00328f3aed00
Nathaniel Jones <nathaniel_jones@selinc.com>
2024-04-30 16:10:22 -0700

    Set up pytest with conftest.py

--- py_acronym_keeper/config.py
+++ py_acronym_keeper/config.py
@@ -6,4 +6,5 @@ from platformdirs import user_data_path
 
 
 REPO_ROOT_DIR: Path = Path(__file__).parents[1]
+REPO_TEST_DIR: Path = REPO_ROOT_DIR / "tests"
 USER_DATA_DIR: Path = user_data_path("py_acronym_keeper", "n8jhj")
