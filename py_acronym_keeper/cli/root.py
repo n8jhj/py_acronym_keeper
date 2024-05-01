6a1060a3209c223a1bec1af4ccaa0856c1314093
Nathaniel Jones <nathaniel_jones@selinc.com>
2024-05-01 09:09:38 -0700

    Add preferred reference and CLI TODOs

--- py_acronym_keeper/cli/root.py
+++ py_acronym_keeper/cli/root.py
@@ -13,3 +13,9 @@ def root_group():
     # Thanks to Patrick Gillespie for the ASCII art:
     # https://patorjk.com/software/taag/#p=display&f=Three%20Point&t=pak
     ...
+
+
+# TODO: pak list kl* : List acronyms starting with "kl"
+# TODO: pak list *p : List acronyms ending with "*p"
+# TODO: pak list -c ONFi : Case-sensitive list of acronyms matching "ONFi" (default insensitive)
+# TODO: pak add ONFi : Prompt for fields of, and add, ONFi
