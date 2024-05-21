0e0e8813186f432550d79968e388cf2eb35d49d2
Nathaniel Jones <nathaniel_jones@selinc.com>
2024-05-20 17:02:24 -0700

    Catch bad length case for Stringer.truncate()

--- py_acronym_keeper/util.py
+++ py_acronym_keeper/util.py
@@ -6,6 +6,9 @@ class Stringer:
 
     @staticmethod
     def truncate(s: str, max_len: int, suffix: str = "...") -> str:
+        if max_len <= len(suffix):
+            raise ValueError(f"max_len ({max_len}) < len(suffix) ({len(suffix)})")
+
         if max_len >= 0 and len(s) > max_len:
             return s[: max_len - len(suffix)] + suffix
         return s
--- tests/test_util_stringer.py
+++ tests/test_util_stringer.py
@@ -6,8 +6,16 @@ from py_acronym_keeper.util import Stringer
 @pytest.mark.parametrize(
     ["s", "max_len", "suffix", "expected"],
     [
-        ("abcdefghijkl", 5, "...", "ab..."),
+        ("abcde", 2, "...", ValueError),
+        ("abcde", 3, "...", ValueError),
+        ("abcde", 4, "...", "a..."),
     ],
 )
-def test_truncate(s: str, max_len: int, suffix: str, expected: str) -> None:
-    assert Stringer.truncate(s, max_len, suffix=suffix) == expected
+def test_truncate(s: str, max_len: int, suffix: str, expected: str | type) -> None:
+    if isinstance(expected, str):
+        assert Stringer.truncate(s, max_len, suffix=suffix) == expected
+    elif issubclass(expected, Exception):
+        with pytest.raises(expected) as e:
+            Stringer.truncate(s, max_len, suffix=suffix)
+    else:
+        assert False, "Bad type for test case."
